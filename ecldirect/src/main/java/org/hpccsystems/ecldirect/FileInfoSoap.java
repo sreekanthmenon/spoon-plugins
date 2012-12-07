c23ba8b5fa6b6a9e194ed8f6b97cc923bd277349
Joseph Chambers <joe.chambers@lexisnexis.com>
2012-12-07 10:45:32 -0500

    deleted folders related to salt that accidently got into master branch

--- ecldirect/src/main/java/org/hpccsystems/ecldirect/FileInfoSoap.java
+++ ecldirect/src/main/java/org/hpccsystems/ecldirect/FileInfoSoap.java
@@ -1,6 +1,9 @@
 package org.hpccsystems.ecldirect;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -124,14 +127,120 @@ public class FileInfoSoap {
 		return null;
 	}
 	
+	public void fetchFileMeta(String fileName){
+	 	String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+	 			"<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+	 			"<soap:Body>" +
+	 			"<DFUBrowseData xmlns=\"urn:hpccsystems:ws:wsdfu\">" +
+	 			"<LogicalName>" + fileName + "</LogicalName>" +
+	 			"<FilterBy></FilterBy>" +
+	 			"<ShowColumns></ShowColumns>" +
+	 			"<SchemaOnly>true</SchemaOnly>" +
+	 			"<StartForGoback></StartForGoback>" +
+	 			"<CountForGoback></CountForGoback>" +
+	 			"<ChooseFile></ChooseFile>" +
+	 			"<Cluster></Cluster>" +
+	 			"<ClusterType></ClusterType>" +
+	 			"<ParentName></ParentName>" +
+	 			"<Start></Start>" +
+	 			"<Count></Count>" +
+	 			"<DisableUppercaseTranslation></DisableUppercaseTranslation>" +
+	 			"</DFUBrowseData>" +
+	 			"</soap:Body>" +
+	 			"</soap:Envelope>";
+	 	
+	 	soap = new ECLSoap();
+		
+		soap.setHostname(serverHost);
+		soap.setPort(this.serverPort);
+		
+		String path = "/WsDfu/DFUBrowseData?ver_=1.1";
+		
+		InputStream is = soap.doSoap(xml, path);
+				
+		/*
+        BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        try {
+			while((line = in.readLine()) != null) {
+			  System.out.println(line);
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		*/
+		try{
+			processFileMeta(is);
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	public ArrayList<String> processFileMeta(InputStream is) throws Exception{
+		ArrayList results = new ArrayList();
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        
+        Document dom = db.parse(is);
+
+        Element docElement = dom.getDocumentElement();
+
+        NodeList dsList = docElement.getElementsByTagName("DFUBrowseDataResponse");
+        
+        
+        if (dsList != null && dsList.getLength() > 0) {
+        	
+           //ArrayList dsArray = new ArrayList();
+
+           //results = dsArray;
+
+            for (int i = 0; i < dsList.getLength(); i++) {
+            	System.out.println("Node:" + dsList.item(i).getNodeName());
+                Element ds = (Element) dsList.item(i);
+                System.out.println(ds.getFirstChild().getNodeName());
+                NodeList rowList = ds.getElementsByTagName("ColumnsHidden");
+                System.out.println("Node:" + rowList.getLength());
+                if (rowList != null && rowList.getLength() > 0) {
+                		
+                	
+                	
+                    ArrayList rowArray = new ArrayList();
+
+                    for (int j = 0; j < rowList.getLength(); j++) {
+                    	System.out.println("Node:");
+                        Element row = (Element) rowList.item(j);
+                        
+                        NodeList columnList = row.getChildNodes();
+                        for (int k = 0; k < columnList.getLength(); k++) {
+                        	System.out.println("Name: " + columnList.item(k).getChildNodes());
+                            
+                        	NodeList colList = columnList.item(k).getChildNodes();
+                        	
+                        	for(int c=0; c<colList.getLength(); c++){
+                        		System.out.println(colList.item(c).getNodeName() + " : " + colList.item(c).getTextContent());
+                        	}
+     
+                        }  
+                    }
+                }
+
+            }
+        }
+        Iterator iterator = results.iterator();
+        return results;
+	}
 	
 	public static void main(String[] args){
 		FileInfoSoap c = new FileInfoSoap("192.168.80.132", 8010);
-		String[] test = c.fetchFiles();
+		/*String[] test = c.fetchFiles();
 		System.out.println("You have " + test.length + " Files");
 		for (int i = 0; i<test.length; i++){
 			System.out.println(test[i]);
-		}
+		}*/
+		
+		c.fetchFileMeta("~thor::foodmart::productsoutput");
 		
 	}
 }
